ea7f3737ceb0408b4aa93dc131d48184a58c5412
NikitaTropin3536 <Tropinikita.06@mail.ru>
2023-02-28 03:37:43 +0300

    It is MyStack

---
+++ src/MyStack.java
@@ -0,0 +1,49 @@
+public class MyStack<T> {
+
+    private Node head;
+    private int size;
+    private class Node {
+        T data;
+        Node next = null;
+
+        public Node(T data) {
+            this.data = data;
+        }
+    }
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+
+    public void push(T data) {
+        Node newNode = new Node(data);
+        if (!isEmpty()) newNode.next = head;
+        head = newNode;
+        ++size;
+    }
+
+    public T peek() {
+        if (isEmpty()) return null;
+        return head.data;
+    }
+
+    public T pop() {
+        if (isEmpty()) return null;
+        T returnData = head.data;
+        head = head.next;
+        --size;
+        return returnData;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        Node cursor = head;
+        while (cursor != null) {
+            builder.append(cursor.data.toString() + "->");
+            cursor = cursor.next;
+        }
+        builder.append("null");
+        return builder.toString();
+    }
+}
